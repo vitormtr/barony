ae1c4b913875267951dea74a9f9dffb2ead4bc83
vitormtr <vitor.triches@gmail.com>
2025-02-25 19:48:14 -0300

    adicionado imagem dos cavalos e criada classe player

--- main.js
+++ main.js
@@ -1,39 +1,4 @@
 
+import { Board } from './Board.js';
 
-const hexContainer = document.getElementById("hexContainer");
-
-const textures = [
-    { type: "water", chance: 0.20 },
-    { type: "mountain", chance: 0.20 },
-    { type: "forest", chance: 0.20 },
-    { type: "farm", chance: 0.20 },
-    { type: "plain", chance: 0.20 }
-];
-
-function getRandomTexture() {
-    const rand = Math.random();
-    let cumulative = 0;
-    for (const texture of textures) {
-        cumulative += texture.chance;
-        if (rand < cumulative) return texture.type;
-    }
-    return "water";
-}
-
-for (let row = 0; row < 10; row++) {
-    const rowContainer = document.createElement("div");
-    rowContainer.classList.add("hex-container");
-
-    if (row % 2 !== 0) {
-        rowContainer.classList.add("offset");
-    }
-    
-    for (let col = 0; col < 11; col++) {
-        const hex = document.createElement("div");
-        hex.classList.add("hexagon");
-        const randomTexture = getRandomTexture();
-        hex.style.backgroundImage = `url(${randomTexture}.png)`;
-        rowContainer.appendChild(hex);
-    }
-    hexContainer.appendChild(rowContainer);
-}
+let board = new Board(10, 10);
