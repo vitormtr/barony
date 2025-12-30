86361aba50eb3c822a2fc602522f0a0dcead2278
vitormtr <vitor.triches@gmail.com>
2025-12-29 23:13:26 -0300

    refactor: remove unused code
    - Remove client/state.js (unused module with broken CONFIG import)
    - Remove dead SOCKET_EVENTS object from server/config.js (never used)

--- server/config.js
+++ server/config.js
@@ -5,18 +5,5 @@ export const config = {
     SOCKET_RECOVERY: {
       maxDisconnectionDuration: 120000,
       skipMiddlewares: true
-    },
-    SOCKET_EVENTS: {
-      CREATE_ROOM: 'createRoom',
-      JOIN_ROOM: 'joinRoom',
-      UPDATE_BOARD: 'updateBoard',
-      CREATE_BOARD: 'createBoard',
-      DRAW_PLAYERS: 'drawPlayers',
-      ERROR: 'error',
-      UPDATE_PLAYER_PIECES: 'updatePlayerPieces',
-      PLAYER_JOINED_ROOM: 'playerJoinedRoom',
-      UPDATED_PLAYER_TEXTURE: 'updateDPlayerTextures',
-      REQUEST_PLAYER_DATA: 'requestPlayerData',
-      PLAYER_DATA_RESPONSE: 'playerDataResponse'
     }
   };
